c30cc8a6bb37efa335ed44284527706ae8667840
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-03-14 20:26:20 +0530

    add express server with multiple routes

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('Hello World!');
+// Create a server
+const express = require('express');
+
+const app = express();
+
+app.get('/test',(req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/test1',(req, res) => {
+  res.send('Hello World1');
+});
+
+app.get('/test2',(req, res) => {
+  res.send('Hello World2');
+});
+
+app.get('/test3',(req, res) => {
+  res.send('Joseph');
+});
+
+app.listen(7777, () => {
+  console.log('Server is listening on 3000');
+});
