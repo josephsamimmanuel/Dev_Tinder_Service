3cb3a1082774cf24ad78dfa2fb3f51985bea1cf2
josephsamimmanuel-dev <josephsamimmanuelselvin.tgi@gmail.com>
2025-03-15 05:45:03 +0530

    add comments outlining project setup and dependencies in homework.js

--- src/homework.js
+++ src/homework.js
@@ -1,3 +1,16 @@
+// - Create a repository
+// - Initialize the repository
+// - node modules, package.json, package-lock.json, .gitignore
+// - Install express
+// - Create a server
+// - Listen to port 7777
+// - Create a request handler for /test
+// - Install nodemon and update scripts inside package.json
+// - What are dependencies?
+// - what is the use of '-g' while npm Install
+// - Difference between carat and tilde (^ and ~) in package.json
+
+
 const express = require('express');
 
 const app = express();
