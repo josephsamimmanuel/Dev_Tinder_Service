fa3a58ae2ca635bde5f27c1fdc7d069022e5f508
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-03-17 00:16:41 +0530

    add user model and signup route with error handling in app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
 const express = require('express')
 const connectDB = require('./config/database')
 const app = express()
+const User = require('./models/user')
+
+app.post('/signup', async(req, res) => {
+  // Create a new user object
+  const userObj = {
+    firstName: 'Sachin',
+    lastName: 'Tendulkar',
+    age: 25,
+    emailId: 'sachintendulkar@gmail.com',
+    password: 'jesuslovesjose4@',
+    gender: 'Male',
+  }
+  // Create a new user instance
+  const user = new User(userObj) 
+  // Save the user to the database
+  try {
+    await user.save()
+    res.send('User created successfully')
+  } catch (error) {
+    res.status(400).send('Error creating user'+error)
+  }
+});
 
 connectDB().then(() => {
   console.log('Connected to MongoDB');
