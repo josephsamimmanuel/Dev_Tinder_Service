22d9c372be807833a28f0c765b70afe3cc85c72d
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-03-16 09:12:29 +0530

    refactor user route handling in app.js to implement multiple middleware functions for improved logging and control flow

--- src/app.js
+++ src/app.js
@@ -3,16 +3,28 @@ const express = require('express');
 
 const app = express();
 
-app.use('/user', (req, res, next) => {
+// app.use('route', rH1, rH2, rH3, rH4);
+
+app.use('/user', [(req, res, next) => {
   // Route Handler
   // res.send('Route Handler 1');
-  console.log('Handling the Router back');
+  console.log('Handling the Router back 1', req.url, req.method);
+  next(); // This will pass the control to the next middleware
   // res.send('Hello from the router: Resposnse 1');
+  // next();
+}, (req, res, next) => {
+  console.log('Handling the Router back 2', req.url, req.method);
+  // res.send('Hello from the router: Response 2');
   next();
-}, (req, res) => {
-  console.log('Handling the Router back');
-  res.send('Hello from the router: Response 2');
-});
+}, (req, res, next) => { // This will not be executed as the response is already sent
+  console.log('Handling the Router back 3', req.url, req.method);
+  // res.send('Hello from the router: Response 3');
+  next();
+}, (req, res, next) => { // This will not be executed as the response is already sent
+  console.log('Handling the Router back 4', req.url, req.method);
+  // res.send('Hello from the router: Response 4');
+  next(); // This will pass the control to the next middleware // Cannot get /user
+}]);
 
 app.listen(7777, () => {
   console.log('Server is listening on 3000');
