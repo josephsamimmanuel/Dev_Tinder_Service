af367214f281f653e3985eafc0195c74c271ac8b
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-03-19 07:46:00 +0530

    add dotenv for environment variables; refactor MongoDB connection and enhance user model validation

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+const MONGO_URL = process.env.MONGO_URL;
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
 const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://josephDev:Bg7wyfTMtcgMAwNu@josephdev.2xopt.mongodb.net/devTinder');
+    await mongoose.connect(MONGO_URL);
 }
 
 module.exports = connectDB;
--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 // Create User Schema and User Models
 
 const mongoose = require('mongoose');
+var validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -24,23 +25,33 @@ const userSchema = new mongoose.Schema({
         lowercase: true,
         required: true,
         unique: true,
-        validate: {
-            validator: function(email) {
-                return /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/.test(email);
-            },
-            message: props => `${props.value} is not a valid email id`
+        // validate: {
+        //     validator: function(email) {
+        //         return /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/.test(email);
+        //     },
+        //     message: props => `${props.value} is not a valid email id`
+        // },
+        validate(email) {
+            if (!validator.isEmail(email)) {
+                throw new Error("Email is not valid");
+            }
         },
         trim: true,
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        validate(password) {
+            if (!validator.isStrongPassword(password)) {
+                throw new Error("Password is not strong");
+            }
+        },
     },
     gender: {
         type: String,
         validate: {
-            validator: function(gender) {
-                if(!["male", "female", "others"].includes(gender.toLowerCase())) {
+            validator: function (gender) {
+                if (!["male", "female", "others"].includes(gender.toLowerCase())) {
                     throw new Error("Gender should be valid")
                 }
             }
@@ -48,7 +59,7 @@ const userSchema = new mongoose.Schema({
     },
     photoUrl: {
         type: String,
-        default: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200'     
+        default: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200'
     },
     about: {
         type: String,
@@ -57,7 +68,7 @@ const userSchema = new mongoose.Schema({
     skills: {
         type: Array,
     },
-}, {timestamps: true});
+}, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
