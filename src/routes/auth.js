99bfcdd6af26e496ae04da7061b952a67785feed
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-05-10 10:22:02 +0530

    Refactor server initialization to use HTTP server; update cookie expiration to 1 hour and log token in login route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -32,9 +32,7 @@ authRouter.post('/signup', async (req, res) => {
       // Add the token to cookies
       res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-        maxAge: 24 * 60 * 60 * 1000 // 24 hours
+        maxAge: 3600000 // 1 hour in milliseconds
       })
       // Send a success response to the client
       res.status(201).json({
